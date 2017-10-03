e6b041444ec436ca209cc1f8d35b5b6cb5ca641c
Noelle <noelledino@gmail.com>
2017-10-03 12:38:43 -0400

    Prototype Commit
    Milestone 1

---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameManager : MonoBehaviour {
+
+	public int points;
+	public Text pointsText;
+
+	void Start () {
+	}
+
+	void Update () {
+
+		pointsText.text = ("" + points + " candy");
+	}
+}
